f90c35a5ddb72f1a603919fa4428598cfb47c244
Khine Zar Thwe <koalakhine@gmail.com>
2023-08-14 16:09:31 +0900

    Adding Polls app tutorial in our repo

--- mmdt/mmdt/urls.py
+++ mmdt/mmdt/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
@@ -22,3 +23,7 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('polls/', include('polls.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns.append(path('__debug__/', include(debug_toolbar.urls)))

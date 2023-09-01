0ab5198c2ad38a77ed60971b20e145829650439a
Htetk Khine <135047040+Htetkkhine@users.noreply.github.com>
2023-09-01 21:59:23 +0630

    adding image

--- mmdt/blog/models.py
+++ mmdt/blog/models.py
@@ -10,6 +10,7 @@ STATUS = (
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
+    header_image = models.ImageField(null=True, blank=True, upload_to="images/")
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now= True)

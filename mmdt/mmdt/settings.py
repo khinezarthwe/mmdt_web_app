bdf6a30587d844deacde178bae6497fa777cbf97
Khine Zar Thwe <koalakhine@gmail.com>
2023-09-02 13:23:31 +0900

    homepage (#9)

--- mmdt/mmdt/settings.py
+++ mmdt/mmdt/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--ld-c!55j+l1gr@alfh&w#+flyd@r*y^wy_r+sokxv&i6d0%tg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mmdt.istarvz.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'crispy_forms',
     'crispy_bootstrap4',
-    'django_summernote'
+    'django_summernote',
+    'django_bootstrap_icons'
 ]
 
 CSRF_TRUSTED_ORIGINS = ['https://*.istarvz.com', 'https://*.127.0.0.1']
@@ -127,7 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

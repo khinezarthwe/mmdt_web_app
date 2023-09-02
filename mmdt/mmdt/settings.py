379bbdfabd507114de1c6e8ca004f05f46be3c6e
Khin Kyaing Kyaing Thein <60054875+khinkyaingkyaingthein@users.noreply.github.com>
2023-09-02 12:00:47 +0630

    add image and views count (#6)
    * add image and views count
    
    * add post detail image
    
    * adding lib and review fix #6
    
    ---------
    
    Co-authored-by: Khine Zar Thwe <koalakhine@gmail.com>

--- mmdt/mmdt/settings.py
+++ mmdt/mmdt/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--ld-c!55j+l1gr@alfh&w#+flyd@r*y^wy_r+sokxv&i6d0%tg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mmdt.istarvz.com']
+ALLOWED_HOSTS = ['mmdt.istarvz.com', '127.0.0.1']
 
 
 # Application definition

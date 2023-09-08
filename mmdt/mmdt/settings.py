50fcd1926840f187a61eb4ee2b709d37e748bc74
Htetk Khine <135047040+Htetkkhine@users.noreply.github.com>
2023-09-08 21:19:50 +0630

    Update settings.py

--- mmdt/mmdt/settings.py
+++ mmdt/mmdt/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'mmdt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sample',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',  # or the hostname where your MySQL server is running
+        'PORT': '3306',      # or the port on which your MySQL server is listening
     }
 }
 

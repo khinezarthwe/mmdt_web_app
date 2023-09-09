cfaa96c5a49f4862b09fc420d0d1df46dadfa79c
Khine Zar Thwe <koalakhine@gmail.com>
2023-09-09 22:07:58 +0900

    adding photos (#14)

--- mmdt/blog/urls.py
+++ mmdt/blog/urls.py
@@ -1,8 +1,9 @@
 from . import views
 from django.urls import path
-from blog.views import Home
+from .views import Home
+
 urlpatterns = [
-    path('',Home.as_view(), name='home'),
+    path('', Home.as_view(), name='home'),
     path('blog/<slug:slug>/', views.PostDetail.post_detail, name='post_detail'),
     path('blog/', views.PostList.post_list, name='post_list')
 ]

01e761358b8b63c8173704ff06ecc674fce8fb06
alex peain <tonghsengsom@gmail.com>
2023-09-16 11:56:19 +0000

    Revert "add script to bin" Delete call.sh and stop_server.sh This reverts commit 07e12b40bdbfb7dd583bc8ee3344beededcf3b77.

--- mmdt/bin/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Activate the virtual environment
-source /home/ubuntu/mmdt-web-app/myenv/bin/activate 
-
-# Change directory to the location of manage.py
-cd /home/ubuntu/mmdt-web-app/mmdt
-python3 manage.py runserver 0.0.0.0:8000

25f7caef6b3a329c7c60b317110bc0703ed6712b
amh23 <65358071+amh23@users.noreply.github.com>
2023-09-20 19:18:27 +0630

    Add about, contact, policy and tableau component (#17)
    * * add blog list page
    * add blog nav item
    * update blog detail page
    
    * add about, contact, policy and tableau component
    
    * #17 adding more file in fix
    
    ---------
    
    Co-authored-by: khinezar <koalakhine@gmail.com>

--- mmdt/blog/urls.py
+++ mmdt/blog/urls.py
@@ -1,9 +1,12 @@
-from . import views
 from django.urls import path
-from .views import Home
+
+from . import views
+from .views import Home, AboutUs, ContactUs
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('blog/<slug:slug>/', views.PostDetail.post_detail, name='post_detail'),
-    path('blog/', views.PostList.post_list, name='post_list')
+    path('blog/', views.PostList.post_list, name='post_list'),
+    path('about/', AboutUs.as_view(), name='about'),
+    path('contact/', ContactUs.as_view(), name='contact')
 ]

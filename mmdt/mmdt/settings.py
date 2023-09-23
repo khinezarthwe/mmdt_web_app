ecbcf4be060198a1ae82d7d74c3853974adbb336
AyeMyaHan <ayemyahan23@gmail.com>
2023-09-23 15:54:10 +0630

    access js,cs,images from Amazon S3, the bucketname is backendsaveimages

--- mmdt/mmdt/settings.py
+++ mmdt/mmdt/settings.py
@@ -162,8 +162,7 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_S3_VERITY = True
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-
+s
 log_dir = os.path.join(BASE_DIR, 'logs')
 if not os.path.exists(log_dir):
     os.makedirs(log_dir)
@@ -192,3 +191,7 @@ LOGGING = {
         'level': 'INFO',
     },
 }
+
+# for static file storage (e.g js, css, images)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+

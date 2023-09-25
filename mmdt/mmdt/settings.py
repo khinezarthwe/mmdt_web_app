9c6342475d1b498c72400b3fa84bb42fb2e58d49
khinezar <koalakhine@gmail.com>
2023-09-25 17:22:50 +0900

    Update settings.py

--- mmdt/mmdt/settings.py
+++ mmdt/mmdt/settings.py
@@ -162,7 +162,7 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_S3_VERITY = True
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-s
+
 log_dir = os.path.join(BASE_DIR, 'logs')
 if not os.path.exists(log_dir):
     os.makedirs(log_dir)

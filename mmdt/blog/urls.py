3fb34e4b5486850996269eac092cdddf9b04410c
khinezar <koalakhine@gmail.com>
2023-10-28 21:31:47 +0900

    Squashed commit of the following:
    commit 10bd7b764bb468c33c12a0875d63ae5ff1871fdf
    Author: khinezar <koalakhine@gmail.com>
    Date:   Sat Oct 28 21:31:26 2023 +0900
    
        fix review
    
    commit f709240749f984eb675a07698c0f028158261121
    Author: khinezar <koalakhine@gmail.com>
    Date:   Sat Oct 28 21:26:05 2023 +0900
    
        update frontend UI
    
    commit 370a23a8a23337d58dfd61dc45d325be3d26e486
    Author: khinezar <koalakhine@gmail.com>
    Date:   Sun Oct 15 23:06:03 2023 +0900
    
        temp

--- mmdt/blog/urls.py
+++ mmdt/blog/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 from . import views
-from .views import Home, AboutUs, ContactUs, OurProject
+from .views import AboutUs, OurProject, StProject
 
 urlpatterns = [
     path('', views.PostList.post_list, name='home'),
     path('blog/<slug:slug>/', views.PostDetail.post_detail, name='post_detail'),
     path('blog/', views.PostList.post_list, name='post_list'),
+    path('st_project/', StProject.as_view(), name='st_projects'),
     path('our_project/', OurProject.as_view(), name='our_projects'),
     path('about/', AboutUs.as_view(), name='about'),
-    path('contact/', ContactUs.as_view(), name='contact')
 ]

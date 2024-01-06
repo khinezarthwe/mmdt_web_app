2f48412ef33556413acac6988df1b747162fe7c4
iceface <www.iceface@gmail.com>
2024-01-06 20:44:45 +0630

    polls pagination ui display

--- mmdt/polls/views.py
+++ mmdt/polls/views.py
@@ -3,16 +3,41 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.contrib import messages
 from .models import Question, Choice
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 
 class PollHomePage:
     def index(request):
         latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+        questions = Question.objects.all()
+
+        paginator = Paginator(questions, 5)
+        page = request.GET.get('page', 1)
+
+        try:
+            page_obj = paginator.page(page)
+
+        except PageNotAnInteger:
+            page_obj = paginator.page(1)
+
+        except EmptyPage:
+            page_obj = paginator.page(paginator.num_pages)
+          
+
         for question in latest_question_list:
             question.choices = question.choice_set.all()
             question.voted = request.GET.get('voted') == 'true'
+
         user_has_voted = request.GET.get('voted') == 'true'
-        context = {'latest_question_list': latest_question_list, 'user_has_voted': user_has_voted}
+
+        context = {
+            'latest_question_list': latest_question_list, 
+            'user_has_voted': user_has_voted,
+            "page": page,
+            "page_obj": page_obj,
+            }
         return render(request, 'polls/index.html', context)
 
     def vote(request):

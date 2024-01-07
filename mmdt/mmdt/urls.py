9ce98378309245de81dd0b8b5040a6eab58abbd0
soewaiyanag <soewaiyanag@gmail.com>
2024-01-07 07:58:40 +0630

    require log-in to access polls page

--- mmdt/mmdt/urls.py
+++ mmdt/mmdt/urls.py
@@ -20,14 +20,15 @@ from django.urls import path, include
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
-    path('polls/', include('polls.urls', namespace='polls')),
-    path('summernote/', include('django_summernote.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("admin/", admin.site.urls),
+    path("", include("blog.urls")),
+    path("polls/", include("polls.urls", namespace="polls")),
+    path("summernote/", include("django_summernote.urls")),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns.append(path('__debug__/', include(debug_toolbar.urls)))
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+
+    urlpatterns.append(path("__debug__/", include(debug_toolbar.urls)))
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mmdt/polls/views.py
+++ mmdt/polls/views.py
@@ -3,39 +3,43 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.contrib import messages
 from .models import Question, Choice
+from django.contrib.auth.decorators import login_required
 
 
 class PollHomePage:
+    @login_required
     def index(request):
-        latest_question_list = Question.objects.order_by('-pub_date')[:5]
+        latest_question_list = Question.objects.order_by("-pub_date")[:5]
         for question in latest_question_list:
             question.choices = question.choice_set.all()
-            question.voted = request.GET.get('voted') == 'true'
-        user_has_voted = request.GET.get('voted') == 'true'
-        context = {'latest_question_list': latest_question_list, 'user_has_voted': user_has_voted}
-        return render(request, 'polls/index.html', context)
+            question.voted = request.GET.get("voted") == "true"
+        user_has_voted = request.GET.get("voted") == "true"
+        context = {
+            "latest_question_list": latest_question_list,
+            "user_has_voted": user_has_voted,
+        }
+        return render(request, "polls/index.html", context)
 
     def vote(request):
-        questions = Question.objects.order_by('-pub_date')[:5]
+        questions = Question.objects.order_by("-pub_date")[:5]
         try:
             for question in questions:
-                selected_choice_id = request.POST.get(f'question_{question.id}')
+                selected_choice_id = request.POST.get(f"question_{question.id}")
                 if not selected_choice_id:
                     # If no choice was selected for a question, raise an error
-                    raise ValueError(f'No choice selected for question "{question.question_text}".')
+                    raise ValueError(
+                        f'No choice selected for question "{question.question_text}".'
+                    )
 
                 selected_choice = question.choice_set.get(pk=selected_choice_id)
                 selected_choice.votes += 1
                 selected_choice.save()
-            if 'vote_again' in request.POST:
-                return HttpResponseRedirect(reverse('polls:index'))
+            if "vote_again" in request.POST:
+                return HttpResponseRedirect(reverse("polls:index"))
             # Redirect with 'voted' flag after successful voting
-            return HttpResponseRedirect(reverse('polls:index') + "?voted=true")
+            return HttpResponseRedirect(reverse("polls:index") + "?voted=true")
 
         except (KeyError, Choice.DoesNotExist, ValueError) as e:
             # Display an error message and redirect back to the index page
             messages.error(request, str(e))
-            return HttpResponseRedirect(reverse('polls:index'))
-
-
-
+            return HttpResponseRedirect(reverse("polls:index"))

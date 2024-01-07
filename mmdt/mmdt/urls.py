e2874e1d6cb2ae1a849903cd1d6e2344ae3d0356
soewaiyanag <soewaiyanag@gmail.com>
2024-01-07 19:01:55 +0630

    Update authentication URLs: Changed from '/accounts/' to just '/login/' and '/logout/'

--- mmdt/mmdt/urls.py
+++ mmdt/mmdt/urls.py
@@ -19,9 +19,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from users.views import register
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path("accounts/", include("django.contrib.auth.urls")),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
     path("register/", register, name="register"),
     path("admin/", admin.site.urls),
     path("", include("blog.urls")),

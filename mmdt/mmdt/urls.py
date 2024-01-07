f1016eb912f24e2bea89dd59efca576aeccde353
soewaiyanag <soewaiyanag@gmail.com>
2024-01-07 11:49:22 +0630

    implemented log-in + register

--- mmdt/mmdt/urls.py
+++ mmdt/mmdt/urls.py
@@ -18,9 +18,11 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from users.views import register
 
 urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
+    path("register/", register, name="register"),
     path("admin/", admin.site.urls),
     path("", include("blog.urls")),
     path("polls/", include("polls.urls", namespace="polls")),

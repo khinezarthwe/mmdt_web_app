2c47b71c25f090ac965cc52f175096422d5963e9
Yout <youtlone95@gmail.com>
2024-01-13 17:47:08 +0630

    Update of views.py to show pagination for all the questions

--- mmdt/polls/views.py
+++ mmdt/polls/views.py
@@ -3,14 +3,34 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.contrib import messages
 from .models import Question, Choice
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 class PollHomePage:
     def index(request):
-        latest_question_list = Question.objects.order_by('-pub_date')[:5]
+        # Get all polls, not just the latest 5
+        all_questions = Question.objects.order_by('-pub_date')
+        
+        # Set the number of polls to display per page
+        polls_per_page = 5
+        paginator = Paginator(all_questions, polls_per_page)
+
+        # Get the current page number from the request's GET parameters
+        page = request.GET.get('page')
+
+        try:
+            latest_question_list = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver the first page.
+            latest_question_list = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver the last page of results.
+            latest_question_list = paginator.page(paginator.num_pages)
+
         for question in latest_question_list:
             question.choices = question.choice_set.all()
             question.voted = request.GET.get('voted') == 'true'
+
         user_has_voted = request.GET.get('voted') == 'true'
         context = {'latest_question_list': latest_question_list, 'user_has_voted': user_has_voted}
         return render(request, 'polls/index.html', context)

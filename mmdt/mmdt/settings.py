0420c303d8564741e97ac33c120a4c678ca54e8f
khinezar <koalakhine@gmail.com>
2024-02-03 20:49:57 +0900

    remove unused code

--- mmdt/mmdt/settings.py
+++ mmdt/mmdt/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
-from django.conf import settings
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.

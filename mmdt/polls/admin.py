dcf89dbcae2f80c6624a1d6034a8b3956f4f5ea5
Phone Pyae Myo <96713095+Pyae24@users.noreply.github.com>
2024-02-03 18:10:18 +0630

    Export Poll Results with CSV from Admin Panel (Secure Downloading Unsure) (#68)
    * done export poll results to csv but not sure secure downloading
    
    * fixed minor issues (added docs for the function and single quote for strings)

--- mmdt/polls/admin.py
+++ mmdt/polls/admin.py
@@ -1,3 +1,5 @@
+import csv
+from django.http import HttpResponse
 from django.contrib import admin
 
 from .models import Question, Choice
@@ -6,6 +8,30 @@ from .models import Question, Choice
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
+
+def export_to_csv(modeladmin, request, queryset):
+    """
+    export_to_csv
+    :param modeladmin: The ModelAdmin instance.
+    :param request: The current request.
+    :param queryset: The queryset containing selected poll results.
+    :return: A CSV file response containing the poll results.
+    """
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="poll_results.csv"'
+    writer = csv.writer(response)
+
+    # Write the headers to csv file
+    writer.writerow(['Question', 'Choice', 'Votes'])
+
+    # Write data
+    for question in queryset:
+        for choice in question.choice_set.all():
+            writer.writerow([question.question_text, choice.choice_text, choice.votes])
+    return response
+
+
+export_to_csv.short_description = 'Export Selected Questions to CSV'
 
 
 class QuestionAdmin(admin.ModelAdmin):
@@ -17,7 +43,7 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display = ["question_text", "pub_date", "was_published_recently", "is_enabled"]
     list_filter = ["pub_date", "is_enabled"]
     search_fields = ["question_text"]
-
+    actions = [export_to_csv]
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)

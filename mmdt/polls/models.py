a2d3b6f55f7c289341f6106ea7b739872deda44c
Yout <youtlone95@gmail.com>
2024-02-15 21:48:01 +0630

    Update migration folder

--- mmdt/polls/models.py
+++ mmdt/polls/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.utils import timezone
 
 class ActiveGroup(models.Model):
-    group_id = models.IntegerField(primary_key=True, default=1)
+    group_id = models.IntegerField(primary_key=True)
     is_active = models.BooleanField(default=False)
 
     def __str__(self):

13d475d8a4ea5f520e1108ade6055277f39ae261
Circle <phonepyaepresent@gmail.com>
2024-02-25 21:23:22 +0630

    feat: survey UI

--- mmdt/survey/admin.py
+++ mmdt/survey/admin.py
@@ -1,16 +1,20 @@
 from django.contrib import admin
 from .models import SurveyForm, SurveyQuestion, Response
 
+# Define an inline admin interface for SurveyQuestion
 class SurveyQuestionInline(admin.TabularInline):
-    model = SurveyQuestion
-    extra = 1
+    model = SurveyQuestion # Link to the SurveyQuestion model
+    extra = 1 # Provide 1 extra blank form when creating a new SurveyForm by default
 
+# Define the admin interface for SurveyForm
 class SurveyFormAdmin(admin.ModelAdmin):
-    inlines = [SurveyQuestionInline]
+    inlines = [SurveyQuestionInline] # Include the inline admin interface for SurveyQuestion
 
+# Define the admin interface for Response
 class ResponseAdmin(admin.ModelAdmin):
-    list_display = ('question', 'text')
+    list_display = ('question', 'text') # Display the 'question' and 'text' fields in the Response list view
 
+# Register the admin interfaces with the admin site
 admin.site.register(SurveyForm, SurveyFormAdmin)
 admin.site.register(SurveyQuestion)
 admin.site.register(Response, ResponseAdmin)
--- mmdt/survey/views.py
+++ mmdt/survey/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
-from .models import SurveyForm
+from django.shortcuts import render, redirect
+from .models import SurveyForm, Response
+from django.core.paginator import Paginator
 
 class SurveyPage:
     def index(request):
-        survey_forms = SurveyForm.objects.all()
-        return render(request, 'survey/index.html', {'survey_forms': survey_forms})
+        # Check if the request method is POST (indicating form submission)
+        if request.method == "POST":
+            # Iterate through the POST data and create a Response object for each response
+            for key, value in request.POST.items():
+                # Check if the key starts with 'response_' to identify survey responses
+                if key.startswith('response_'):
+                    # Extract the question ID from the key
+                    question_id = key.split('_')[1]
+                    text = value # The actual response text
+                    # If the response text is not empty, create a new Response object
+                    if text:
+                        Response.objects.create(question_id=question_id, text=text)
+            # Redirect to the survey page
+            return redirect('/survey')
+        # Fetch active survey forms and prefetch related questions
+        active_forms = SurveyForm.objects.filter(is_active=True).prefetch_related('questions')
+        # Pagination or future use
+        paginator = Paginator(active_forms, 5)
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        context = {'page_obj': page_obj}
+        return render(request, 'survey/index.html', context)

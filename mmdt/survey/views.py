405d9d1e5ad38ba1a611bd7fd1b4bec8ee01395b
Yout <youtlone95@gmail.com>
2024-03-04 22:26:20 +0630

    Update of views.py for the checkbox questions type to show correctly

--- mmdt/survey/views.py
+++ mmdt/survey/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Survey, Response, Question
+from .models import Survey, Response, Question, Choice  # Import Choice model
 from .forms import create_survey_form
+from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 class SurveyPage:
     def index(request):
@@ -9,16 +11,56 @@ class SurveyPage:
 
     def survey_detail(request, survey_id):
         survey = get_object_or_404(Survey, pk=survey_id)
+        questions = survey.questions.all().order_by('pub_date')
+
+        # Set the number of questions to display per page
+        questions_per_page = 5
+        paginator = Paginator(questions, questions_per_page)
+
+        # Get the current page number from the request's GET parameters
+        page = request.GET.get('page')
+
+        try:
+            current_page_questions = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver the first page.
+            current_page_questions = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range, deliver the last page of results.
+            current_page_questions = paginator.page(paginator.num_pages)
+
         SurveyForm = create_survey_form(survey)
-        
+
         if request.method == "POST":
             form = SurveyForm(request.POST)
             if form.is_valid():
-                for question in survey.questions.all():
+                for question in current_page_questions:
                     response_text = form.cleaned_data.get(f'question_{question.id}')
-                    if response_text:
+
+                    if response_text is not None:
+                        if question.question_type == Question.CHECKBOX:
+                            # For checkbox questions, response_text is a list
+                            choices = Choice.objects.filter(id__in=response_text)
+                            response_text = ", ".join(choice.choice_text for choice in choices)
+                        elif question.question_type in [Question.MULTIPLE_CHOICE]:
+                            # For other multiple-choice questions, response_text is a single choice
+                            choice_id = int(response_text)
+                            choice = get_object_or_404(Choice, id=choice_id)
+                            response_text = choice.choice_text
+
+                        # Create Response object
                         Response.objects.create(question=question, response_text=response_text)
+
+                # Display success message
+                messages.success(request, 'Survey submitted successfully!')
+
                 return redirect('survey:index')
         else:
             form = SurveyForm()
-        return render(request, 'survey/survey_detail.html', {'survey': survey, 'form': form})
+
+        context = {
+            'survey': survey,
+            'form': form,
+            'current_page_questions': current_page_questions,
+        }
+        return render(request, 'survey/survey_detail.html', context)

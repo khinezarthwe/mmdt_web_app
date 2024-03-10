eb7fe108d58625b2e8910b086be5619aea9ce9f1
Circle <phonepyaepresent@gmail.com>
2024-03-11 02:18:43 +0700

    error: Form Submit button fixed but clicking pagination number also submitting

--- mmdt/survey/views.py
+++ mmdt/survey/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 from .models import Survey, Response, Question, Choice
 from .forms import create_survey_form
 from django.contrib import messages
@@ -13,62 +15,55 @@ class SurveyPage:
         survey = get_object_or_404(Survey, pk=survey_id)
         questions = survey.questions.all().order_by('pub_date')
 
-        # Set the number of questions to display per page
         questions_per_page = 5
         paginator = Paginator(questions, questions_per_page)
-
-        # Get the current page number from the request's GET parameters
         page = request.GET.get('page')
 
         try:
             current_page_questions = paginator.page(page)
         except PageNotAnInteger:
-            # If page is not an integer, deliver the first page.
             current_page_questions = paginator.page(1)
         except EmptyPage:
-            # If page is out of range, deliver the last page of results.
             current_page_questions = paginator.page(paginator.num_pages)
 
         SurveyForm = create_survey_form(survey)
-
+        
         if request.method == "POST":
             form = SurveyForm(request.POST)
             if form.is_valid():
+                action = request.POST.get('action', 'submit')
+                responses = request.session.get('survey_responses', {})
+                
                 for question in current_page_questions:
                     response_text = form.cleaned_data.get(f'question_{question.id}')
-
                     if response_text is not None:
-                        if question.question_type == Question.CHECKBOX:
-                            # For checkbox questions, response_text is a list
-                            choices = Choice.objects.filter(id__in=response_text)
-                            response_text = ", ".join(choice.choice_text for choice in choices)
+                        responses[f'question_{question.id}'] = response_text
 
-                        elif question.question_type == Question.MULTIPLE_CHOICE:
-                            # For Multiple Choice questions, response_text is the selected choice ID
-                            choice_id = int(response_text)
-                            selected_choice = get_object_or_404(Choice, id=choice_id)
-                            response_text = selected_choice.choice_text
-
-                        elif question.question_type == Question.SLIDING_SCALE:
-                            selected_index = int(response_text)
-                            selected_choice = question.choices.all()[selected_index]
-                            response_text = selected_choice.choice_text
-                            print(selected_index)
-
-                        elif question.question_type == Question.DROPDOWN: 
-                            # For drop-down questions, response_text is the selected choice text
-                            choice = get_object_or_404(Choice, id=response_text)
-                            response_text = choice.choice_text
-
-                        # Create Response object
-                        Response.objects.create(question=question, response_text=response_text)
-
-                # Display success message
-                messages.success(request, 'Survey submitted successfully!')
-
-                return redirect('survey:index')
+                request.session['survey_responses'] = responses
+                
+                if action == 'next' and current_page_questions.has_next():
+                    next_page_number = current_page_questions.next_page_number()
+                    redirect_url = reverse('survey:survey_detail', args=[survey_id]) + f'?page={next_page_number}'
+                    return HttpResponseRedirect(redirect_url)
+                elif action == 'previous' and current_page_questions.has_previous():
+                    prev_page_number = current_page_questions.previous_page_number()
+                    redirect_url = reverse('survey:survey_detail', args=[survey_id]) + f'?page={prev_page_number}'
+                    return HttpResponseRedirect(redirect_url)
+                elif action == 'submit' and not current_page_questions.has_next():
+                    # Process all responses from the session
+                    for question_id, response_text in responses.items():
+                        question_id_num = int(question_id.split('_')[-1])
+                        question = get_object_or_404(Question, id=question_id_num)
+                        Response.objects.create(
+                            question=question,
+                            response_text=response_text
+                        )
+                    
+                    messages.success(request, 'Survey submitted successfully!')
+                    del request.session['survey_responses']
+                    return redirect('survey:index')
         else:
-            form = SurveyForm()
+            form = SurveyForm(initial=request.session.get('survey_responses', {}))
 
         context = {
             'survey': survey,

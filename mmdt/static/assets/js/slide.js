140f6baac284e4bc8e17241f17bf338e28e04de4
Yout <youtlone95@gmail.com>
2024-03-10 13:04:30 +0630

    By add slide.js for the slide ui

--- mmdt/static/assets/js/slide.js
+++ mmdt/static/assets/js/slide.js
@@ -1,15 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const sliders = document.querySelectorAll('[id^="slider"]');
+    const sliders = document.querySelectorAll('.slider');
 
     sliders.forEach(function (slider) {
-        const questionId = slider.id.replace('slider', '');
-        const output = document.getElementById(`output${questionId}`);
-        const colorChoices = document.getElementById(`colorChoices${questionId}`);
+        const output = slider.nextElementSibling;
 
         slider.addEventListener('input', function () {
-            const selectedIndex = slider.value;
-            const selectedChoice = colorChoices.children[selectedIndex];
-            output.textContent = selectedChoice.dataset.choiceText;
+            const selectedIndex = slider.value;            
+            // Reset other sliders to their initial state
+            sliders.forEach(function (otherSlider) {
+                if (otherSlider !== slider) {
+                    otherSlider.value = 0;
+                    const otherOutput = otherSlider.nextElementSibling;
+                    otherOutput.textContent = '0%';
+                    otherSlider.style.background = 'linear-gradient(to right, #003399 0%, #003399 0%, #f5f6f7 0%, #f5f6f7 100%)';
+                }
+            });
+
+            // Update the current slider's position
+            output.textContent = `${selectedIndex * 10}%`;
+            slider.style.background = 'linear-gradient(to right, #003399 0%, #003399 ' + (selectedIndex * 10) + '%, #f5f6f7 ' + (selectedIndex * 10) + '%, #f5f6f7 100%)';
         });
     });
 });

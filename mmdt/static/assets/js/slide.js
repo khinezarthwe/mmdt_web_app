9e68f0f473b4f5de3cb8dcbb15655c39e67d8e53
Yout <youtlone95@gmail.com>
2024-03-11 22:34:49 +0630

    Update of slide.js for the SS question type

--- mmdt/static/assets/js/slide.js
+++ mmdt/static/assets/js/slide.js
@@ -6,16 +6,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
         slider.addEventListener('input', function () {
             const selectedIndex = slider.value;            
-            // Reset other sliders to their initial state
-            sliders.forEach(function (otherSlider) {
-                if (otherSlider !== slider) {
-                    otherSlider.value = 0;
-                    const otherOutput = otherSlider.nextElementSibling;
-                    otherOutput.textContent = '0%';
-                    otherSlider.style.background = 'linear-gradient(to right, #003399 0%, #003399 0%, #f5f6f7 0%, #f5f6f7 100%)';
-                }
-            });
-
             // Update the current slider's position
             output.textContent = `${selectedIndex * 10}%`;
             slider.style.background = 'linear-gradient(to right, #003399 0%, #003399 ' + (selectedIndex * 10) + '%, #f5f6f7 ' + (selectedIndex * 10) + '%, #f5f6f7 100%)';

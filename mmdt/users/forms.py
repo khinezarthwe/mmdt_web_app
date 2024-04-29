ce688cf5143d1cca473f42e8fa08e233cadfffcb
dreamingblackcat <dreamingblackcat10@gmail.com>
2024-04-29 19:09:56 +0900

    Fix register page to have a more standard bootstrap form fields

--- mmdt/users/forms.py
+++ mmdt/users/forms.py
@@ -6,3 +6,11 @@ from django.contrib.auth.forms import UserCreationForm
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         fields = UserCreationForm.Meta.fields + ("email",)
+
+    def __init__(self, *args, **kwargs):
+        super(UserCreationForm, self).__init__(*args, **kwargs)
+
+        self.fields["username"].widget.attrs["class"] = "form-control"
+        self.fields["email"].widget.attrs["class"] = "form-control"
+        self.fields["password1"].widget.attrs["class"] = "form-control"
+        self.fields["password2"].widget.attrs["class"] = "form-control"

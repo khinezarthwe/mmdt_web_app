1efdb384dfbbbb11eda3a5ebc446441d35ccae55
dreamingblackcat <dreamingblackcat10@gmail.com>
2024-05-19 20:14:45 +0900

    Make sure response initial value selections are null safe

--- mmdt/survey/forms.py
+++ mmdt/survey/forms.py
@@ -8,35 +8,55 @@ def create_survey_form(survey, user_survey_response, current_page_questions):
     response_by_question = {}
     for r in responses:
         if r.question_id not in response_by_question:
-            response_by_question[r.question_id] = []
-        response_by_question[r.question_id].append(r)
+            response_by_question[r.question_id] = r
     class SurveyForm(forms.Form):
         def __init__(self, *args, **kwargs):
             super(SurveyForm, self).__init__(*args, **kwargs)
             for question in current_page_questions:
                 field_name = f'question_{question.id}'
-                existing_response = response_by_question.get(question.id, [None])[0]
+                existing_response = response_by_question.get(question.id, None)
                 if question.question_type == Question.TEXT:
-                    initial_value = existing_response.response_text if existing_response else None
+                    initial_value = self.get_initial_value_for_text(existing_response)
                     self.fields[field_name] = forms.CharField(
                         label=question.question_text, required=not question.optional, initial=initial_value)
                 elif question.question_type == Question.MULTIPLE_CHOICE:
                     choices = [(choice.id, choice.choice_text) for choice in question.choices.all()]
-                    initial_value = existing_response.choices.first().id if existing_response else None
+                    initial_value = self.get_initial_value_for_single_choice(existing_response)
                     self.fields[field_name] = forms.ChoiceField(choices=choices, label=question.question_text, widget=forms.RadioSelect, required=not question.optional, initial=initial_value)
                 elif question.question_type == Question.CHECKBOX:
                     choices = [(choice.id, choice.choice_text) for choice in question.choices.all()]
-                    initial_values = [c.id for c in existing_response.choices.all()]
+                    initial_values = self.get_initial_value_for_multiple_choice(existing_response)
                     self.fields[field_name] = forms.MultipleChoiceField(choices=choices, label=question.question_text, widget=forms.CheckboxSelectMultiple, required=not question.optional, initial=initial_values)
                 elif question.question_type == Question.LONG_TEXT:
-                    initial_value = existing_response.response_text if existing_response else None
+                    initial_value = self.get_initial_value_for_text(existing_response)
                     self.fields[field_name] = forms.CharField(widget=forms.Textarea, label=question.question_text, required=not question.optional, initial=initial_value)
                 elif question.question_type == Question.DROPDOWN:
                     initial_value = existing_response.choices.first().id if existing_response else None
                     choices = [(choice.id, choice.choice_text) for choice in question.choices.all()]
                     self.fields[field_name] = forms.ChoiceField(choices=choices, label=question.question_text, required=not question.optional, initial=initial_value)
                 elif question.question_type == Question.SLIDING_SCALE:
-                    choices = [(choice.id, choice.choice_text) for choice in question.choices.all()]
-                    initial_value = existing_response.response_text if existing_response else None
+                    initial_value = self.get_initial_value_for_single_choice(existing_response)
                     self.fields[field_name] = forms.IntegerField(label=question.question_text, widget=forms.NumberInput(attrs={'type': 'range'}), required=not question.optional, initial=initial_value)
+
+        def get_initial_value_for_single_choice(self, existing_response):
+            if existing_response is None:
+                return None
+
+            selected_choice = existing_response.choices.first()
+            if selected_choice  is None:
+                return None
+
+            return selected_choice.id
+
+        def get_initial_value_for_text(self, existing_response):
+            if existing_response is None:
+                return None
+            return existing_response.response_text
+
+
+        def get_initial_value_for_multiple_choice(self, existing_response):
+            if existing_response is None:
+                return None
+            return [c.id for c in existing_response.choices.all()]
+
     return SurveyForm

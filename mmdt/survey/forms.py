ddf783c4b584cc53262ba6eddf6a5bd42027a9c1
khinezar <koalakhine@gmail.com>
2024-07-11 10:45:37 +0900

    adding html string for question text

--- mmdt/survey/forms.py
+++ mmdt/survey/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Question
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.utils.safestring import mark_safe
 
 def create_survey_form(survey, user_survey_response, current_page_questions):
     responses = user_survey_response.responses.all() if user_survey_response else []
@@ -9,34 +10,48 @@ def create_survey_form(survey, user_survey_response, current_page_questions):
     for r in responses:
         if r.question_id not in response_by_question:
             response_by_question[r.question_id] = r
+
     class SurveyForm(forms.Form):
         def __init__(self, *args, **kwargs):
             super(SurveyForm, self).__init__(*args, **kwargs)
             for question in current_page_questions:
                 field_name = f'question_{question.id}'
                 existing_response = response_by_question.get(question.id, None)
+                label = mark_safe(question.question_text)
                 if question.question_type == Question.TEXT:
                     initial_value = self.get_initial_value_for_text(existing_response)
                     self.fields[field_name] = forms.CharField(
-                        label=question.question_text, required=not question.optional, initial=initial_value)
+                        label=label, required=not question.optional, initial=initial_value)
                 elif question.question_type == Question.MULTIPLE_CHOICE:
                     choices = [(choice.id, choice.choice_text) for choice in question.choices.all()]
                     initial_value = self.get_initial_value_for_single_choice(existing_response)
-                    self.fields[field_name] = forms.ChoiceField(choices=choices, label=question.question_text, widget=forms.RadioSelect, required=not question.optional, initial=initial_value)
+                    self.fields[field_name] = forms.ChoiceField(choices=choices, label=label,
+                                                                widget=forms.RadioSelect,
+                                                                required=not question.optional,
+                                                                initial=initial_value)
                 elif question.question_type == Question.CHECKBOX:
                     choices = [(choice.id, choice.choice_text) for choice in question.choices.all()]
                     initial_values = self.get_initial_value_for_multiple_choice(existing_response)
-                    self.fields[field_name] = forms.MultipleChoiceField(choices=choices, label=question.question_text, widget=forms.CheckboxSelectMultiple, required=not question.optional, initial=initial_values)
+                    self.fields[field_name] = forms.MultipleChoiceField(choices=choices, label=label,
+                                                                        widget=forms.CheckboxSelectMultiple,
+                                                                        required=not question.optional,
+                                                                        initial=initial_values)
                 elif question.question_type == Question.LONG_TEXT:
                     initial_value = self.get_initial_value_for_text(existing_response)
-                    self.fields[field_name] = forms.CharField(widget=forms.Textarea, label=question.question_text, required=not question.optional, initial=initial_value)
+                    self.fields[field_name] = forms.CharField(widget=forms.Textarea, label=label,
+                                                              required=not question.optional, initial=initial_value)
                 elif question.question_type == Question.DROPDOWN:
                     initial_value = existing_response.choices.first().id if existing_response else None
                     choices = [(choice.id, choice.choice_text) for choice in question.choices.all()]
-                    self.fields[field_name] = forms.ChoiceField(choices=choices, label=question.question_text, required=not question.optional, initial=initial_value)
+                    self.fields[field_name] = forms.ChoiceField(choices=choices, label=label,
+                                                                required=not question.optional,
+                                                                initial=initial_value)
                 elif question.question_type == Question.SLIDING_SCALE:
                     initial_value = self.get_initial_value_for_single_choice(existing_response)
-                    self.fields[field_name] = forms.IntegerField(label=question.question_text, widget=forms.NumberInput(attrs={'type': 'range'}), required=not question.optional, initial=initial_value)
+                    self.fields[field_name] = forms.IntegerField(label=label,
+                                                                 widget=forms.NumberInput(attrs={'type': 'range'}),
+                                                                 required=not question.optional,
+                                                                 initial=initial_value)
 
         def get_initial_value_for_single_choice(self, existing_response):
             if existing_response is None:
